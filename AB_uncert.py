edf7e3a31c4d192da9d9ac166d583fd92b45c0b9
Log-Dog012 <Log-Dog012@noreply.gitcode.com>
2025-11-01 22:19:03 +0800

    输入转换
    当输入为数字列表时转换为数组

--- AB_uncert.py
+++ AB_uncert.py
@@ -5,6 +5,37 @@
 from . import *
 from numpy import asarray
 
+def 数字列表转arr(数字列表):
+    """
+    将数字列表转换为numpy数组。
+
+    参数:
+    数字列表: 一组数字。
+
+    返回:
+    numpy.ndarray: 转换后的数组。
+    """
+    if isinstance(数字列表, (list, tuple)) and all(isinstance(x, (int, float)) for x in 数字列表):
+        return asarray(数字列表)
+    else:
+        return 数字列表
+    
+def 输入转换(func):
+    """
+    装饰器：将输入调整为不确定度合成所需类型。
+
+    参数:
+    func: 需要装饰的函数。
+
+    返回:
+    function: 装饰后的函数。
+    """
+    def wrapper(*分量):
+        新分量 = [数字列表转arr(i) for i in 分量]
+        return func(*新分量)
+    return wrapper
+
+@输入转换
 def 求A类不确定度(测量值列表):
     """
     计算A类不确定度，基于测量值的标准偏差。
@@ -41,37 +72,7 @@ def 仪器误差限转B类不确定度(仪器误差限, K=3**0.5):
 
 InstErr_to_B_uncert = 仪器误差限转B类不确定度
 
-def 数字列表转arr(数字列表):
-    """
-    将数字列表转换为numpy数组。
-
-    参数:
-    数字列表: 一组数字。
-
-    返回:
-    numpy.ndarray: 转换后的数组。
-    """
-    if isinstance(数字列表, (list, tuple)) and all(isinstance(x, (int, float)) for x in 数字列表):
-        return asarray(数字列表)
-    else:
-        return 数字列表
-    
-def 输入检查(func):
-    """
-    装饰器：将输入调整为不确定度合成所需类型。
-
-    参数:
-    func: 需要装饰的函数。
-
-    返回:
-    function: 装饰后的函数。
-    """
-    def wrapper(A分量, B分量):
-        A分量, B分量 = 数字列表转arr(A分量), 数字列表转arr(B分量)
-        return func(A分量, B分量)
-    return wrapper
-
-@输入检查
+@输入转换
 def 不确定度合成(A分量, B分量):
     """
     计算不确定度合成，基于各分量的不确定度平方和的平方根。
