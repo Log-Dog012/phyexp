dc6929a770797027c92f7fe149001d0e753b8c63
Log-Dog012 <Log-Dog012@noreply.gitcode.com>
2026-01-03 17:27:52 +0800

    相对引用

--- phyexp/meas.py
+++ phyexp/meas.py
@@ -2,7 +2,7 @@
 测量量
 """
 
-from .utils import *
+from .utils import ureg, Q_, ufloat
 from typing import Sequence
 from .AB_uncert import 求A类不确定度, 不确定度合成
 from uncertainties.core import UFloat, AffineScalarFunc
@@ -16,11 +16,9 @@ def 一次测量结果(数值, 单位: str = "", B类不确定度: float = 0.0, 
     return obj
 
 
-def 多次测量结果(
-    数值列表: Sequence[float], 单位: str = "", B类不确定度: float = 0.0, 名称: str = ""
-):
+def 多次测量结果(数值列表, 单位: str = "", B类不确定度: float = 0.0, 名称: str = ""):
     # 多次测量结果的不确定度等于A类不确定度与B类不确定度合成
-    数值列表 = np.asarray(数值列表)
+    数值列表 = 数值列表.magnitude if isinstance(数值列表, Q_) else np.array(数值列表)
     A类不确定度 = 求A类不确定度(数值列表)
     不确定度 = 不确定度合成(A类不确定度, B类不确定度)
     平均值 = 数值列表.mean()
